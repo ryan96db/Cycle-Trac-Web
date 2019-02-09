77e1a39e02c4b01c5f5f2020e1efbaa567e88fe5
Ryan DeBose-Boyd <ryan96db@gmail.com>
2019-02-09 17:16:32 -0600

    Add devise authentification.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def home
+  	redirect_to 'pages#home'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_scope :user do
+  	get 'login', to: 'devise/sessions#new'
+  	root 'workouts#index'
+  end
+
+  devise_scope :user do
+  	get 'signup', to: 'devise/registrations#new'
+  end
+
   resources :workouts do
   	resources :songs
   end
-  root 'workouts#index'
+  
 
 end
